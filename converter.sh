ef0ed52cc7049f0400f7e4d4f94561abcecd1989
Eric Githinji <ericgithinji@gmail.com>
2022-07-01 13:30:56 +0300

    First commit

---
+++ converter.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+target=$( find -name *.$1 | wc -l )
+completed=0
+
+function convert_videos {
+    for file in *; do
+        filename=${file%%.*}
+        extension=${file##*.}
+        if [ $extension = "$1" ]; then
+            original=$file
+            converted="${filename}.$2"
+            ffmpeg -i "$original" -c:v copy -c:a copy -y "$converted" 2>> /dev/null 
+            mv "$converted" ../Converted/
+            let completed++
+            echo $((100*$completed/$target))% complete
+        fi
+    done;
+}
+
+echo "Total files to convert: $target"
+mkdir Converted
+for d in */; do
+    cd "$d"
+    echo "converting files in $d"
+    convert_videos "$@"
+    cd ..
+    echo "Files converted: $( find -name *.$2 | wc -l )"
+done;
+
+echo "Done. Total files converted: $( find -name *.$2 | wc -l )"
